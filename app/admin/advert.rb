aad6cd2009d4dd951f495cccf45081cbed172de4
Brad Kingon <bkingon@gmail.com>
2016-01-24 22:18:33 +0200

    added active admin styling back in

--- app/admin/advert.rb
+++ app/admin/advert.rb
@@ -3,7 +3,7 @@ menu priority: 3
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :title, :body, :contact_name, :contact_email, :contact_number, :contact_address, :start_month, :end_month
 #
 # or
 #
